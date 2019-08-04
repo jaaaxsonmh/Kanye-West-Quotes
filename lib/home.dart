59ed56b0a2ec7045deb9b858e51899a5dae33fdd
Jack hosking <jackhosking2@outlook.com>
2019-08-04 14:18:13 +1200

    refactor for building bigger styles

--- lib/home.dart
+++ lib/home.dart
@@ -5,14 +5,13 @@ import 'package:http/http.dart' as http;
 import 'package:kanye_quotes/api/quote_data.dart';
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   String _quote = "Loading...";
+
   @override
   void initState() {
     super.initState();
@@ -23,28 +22,45 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body: new Center(child: new Column(children: <Widget>[new Center(child: new Text(_quote)), new FlatButton(onPressed: getQuote, child: new Text("new quote"))], mainAxisAlignment: MainAxisAlignment.center,),),
+      body: new Center(
+        child: new Column(
+          children: <Widget>[
+            _buildQuoteWidget(),
+            _buildButtonWidget()
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
     );
+  }
+
+  Widget _buildQuoteWidget() {
+    return new Center(child: new Text(_quote));
+  }
+
+  Widget _buildButtonWidget() {
+    return new FlatButton(onPressed: getQuote, child: new Text("new quote"));
   }
 
   void getQuote() {
     findNew().then((val) => setState(() {
-      _quote = val;
-    }));
+          _quote = val;
+        }));
   }
 
   Future<String> findNew() async {
     String kanyeQuoteAPI = 'https://api.kanye.rest/';
 
-      http.Response response = await http.get(kanyeQuoteAPI);
+    http.Response response = await http.get(kanyeQuoteAPI);
 
-      var responseBody = QuoteData.fromJson(jsonDecode(response.body));
-      final httpStatus = response.statusCode;
+    var responseBody = QuoteData.fromJson(jsonDecode(response.body));
+    final httpStatus = response.statusCode;
 
-      if(httpStatus != 200 || httpStatus == null) {
-        throw new GetDataException("Error in http code: $httpStatus");
-      }
+    if (httpStatus != 200 || httpStatus == null) {
+      throw new GetDataException("Error in http code: $httpStatus");
+    }
 
     return responseBody.quote;
   }
+
 }
